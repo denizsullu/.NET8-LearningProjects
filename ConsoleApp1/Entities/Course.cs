f2598b9bdc56b03b12370a69b59781e65d75b415
deniz <denizsullu@protonmail.com>
2024-02-04 22:17:20 +0300

    create course class

---
+++ ConsoleApp1/Entities/Course.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp1.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1.Entities;
+
 Console.WriteLine("Hello, World! deniz");
 Console.WriteLine("deneme ilk yorum .NET heyooo");
 
@@ -10,3 +12,29 @@ double amont = 1000;
 bool isAuthenticated = true;
 // variables --> camelCase 
 Console.WriteLine(isAuthenticated);
+
+// condition
+
+if (isAuthenticated)
+{
+    Console.WriteLine("Hoşgeldin engin");
+}
+else
+{
+    Console.WriteLine("sisteme giriş yap");
+}
+string[] loans = {"kredi1","kredi2","kredi3","kredi4","kredi5","kredi6"};
+string[] loans2 = new string[2];
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "C#";
+course1.Description = ".Net 8";
+course1.Price = 100;
+
+Course[] courses = {course1};
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+}
