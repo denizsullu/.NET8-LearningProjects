5476d32a8fe3dba2ac5b9aa89333e72f13828478
deniz <denizsullu@protonmail.com>
2024-02-04 21:25:14 +0300

    firs commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World! deniz");
+Console.WriteLine("deneme ilk yorum .NET heyooo");
+
+string message = "krediler";
+string message2 = "krediler2";
+int term = 12;
+double amont = 1000;
+bool isAuthenticated = true;
+// variables --> camelCase 
+Console.WriteLine(isAuthenticated);
